f3faa53aaadbd2b3cd898007c4c6095e48b1636f
yamin <gherbi.yamin.y@gmail.com>
2022-03-23 22:33:31 +0100

    refacto

--- src/adapters/toUser.ts
+++ src/adapters/toUser.ts
@@ -1,4 +1,20 @@
-export const normalize = (e: any) => {
+export interface DiscordMember {
+    guildId: string,
+    joinedTimestamp: number,
+    premiumSinceTimestamp: string,
+    nickname: string,
+    pending: boolean,
+    communicationDisabledUntilTimestamp: string,
+    userId: string,
+    avatar: string,
+    displayName: string,
+    roles: string[],
+    avatarURL: string,
+    displayAvatarURL: string,
+}
+
+
+export const normalize = (e: any): DiscordMember => {
     return {
         ...e,
         roles: e.roles.toString()
--- src/services/getUsersByRoles.ts
+++ src/services/getUsersByRoles.ts
@@ -1,8 +1,9 @@
 import type { GuildMember, Guild } from 'discord.js'
 import { normalize } from '../adapters/toUser'
+import type { DiscordMember } from '../adapters/toUser'
 
 export const getUsersByRoles = async (guild: Guild) => {
     const fetchMembers = await guild.members.fetch()
-    const members = fetchMembers?.map((member: GuildMember) => member.toJSON()).map((e: any) => normalize(e)).filter((member: any) => member.roles.includes('955209655829815379'))
+    const members = fetchMembers?.map((member: GuildMember) => member.toJSON()).map((e: any) => normalize(e)).filter((member: DiscordMember) => member.roles.includes('955209655829815379'))
     return members
 }
