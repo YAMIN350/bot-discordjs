cc383707d4848c0869ae405951d3eb331d45fb80
yamin <gherbi.yamin.y@gmail.com>
2022-03-23 22:20:53 +0100

    refacto

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import Discord from 'discord.js'
 import type { GuildMember, Guild } from 'discord.js'
 import xlsx from 'xlsx'
+import type { WorkSheet } from 'xlsx'
 
 const client = new Discord.Client({
     intents: [
@@ -16,21 +17,25 @@ client.on("ready", async(bot) => {
     console.log("Bot online!");
     const guild = bot.guilds.cache.get(`${process.env.BOT_ID}`);
     if(guild) {
-        const data = await getUsersByRoles(guild)
-        convertJsonToExcel(data)
+        const members = await getUsersByRoles(guild)
+        const data = convertJsonToExcel(members)
+        writeExcel(data)
     } else {
         console.log('Guild is not defined !');
     }
 
 })
+const convertJsonToExcel = (data: unknown[]): WorkSheet => {
+    return xlsx.utils.json_to_sheet(data);
+}
 
-const convertJsonToExcel = (data: unknown[]) => {
-    const worksheet = xlsx.utils.json_to_sheet(data);
+const writeExcel = (worksheet: WorkSheet) => {
     const workbook = xlsx.utils.book_new();
     xlsx.utils.book_append_sheet(workbook, worksheet, 'dataTest')
     xlsx.write(workbook, {bookType: 'xlsx', type: 'buffer'})
     xlsx.write(workbook, {bookType: 'xlsx', type: 'binary'})
     xlsx.writeFile(workbook, `${process.env.FILENAME_EXCEL}.xlsx`)
+    console.log('file generated !')
 }
 
 const getUsersByRoles = async (guild: Guild) => {
